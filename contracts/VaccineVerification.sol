f5a8b5d9f9783a5e037f5d750c1e4c017dab21d9
dongh <howard.dong18@gmail.com>
2021-04-05 13:24:29 -0400

    Wrote contract

--- contracts/VaccineVerification.sol
+++ contracts/VaccineVerification.sol
@@ -2,46 +2,56 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract VaccineVerification {
     //    uint public taskCount = 0;
-    address provider;
 
-    struct record {
+    struct Record {
         string date;
         string brand;
         address userAddress;
+        address provider;
     }
 
-    mapping(address => record) public records;
+    mapping(address => Record[]) public records;
 
-
-    event RecordCreated (
+    event EmitRecord(
         string date,
         string brand,
-        address userAddress
+        address userAddress,
+        address provider
     );
 
-    constructor() public{
-        provider = msg.sender;
-        //        provider = random;
+    constructor() public {}
+
+    function createRecord(
+        string memory _date,
+        string memory _brand,
+        address _userAddress,
+        address _provider
+    ) public {
+        records[_userAddress].push(
+            Record(_date, _brand, _userAddress, _provider)
+        );
+        emit EmitRecord(_date, _brand, _userAddress, _provider);
     }
 
-
-
-    function createRecord(string memory _date, string memory _brand, address _userAddress) public {
-        require(msg.sender == provider);
-        records[_userAddress] = record(_date, _brand, _userAddress);
-//        emit RecordCreated(_date, _type, _userAddress, true);
-    }
-
-    function accessRecord() public{
-//        if (records[msg.sender] != null) {
-//            //Definitely not the right way to emit it
-//            emit RecordCreated(_date, _type, _userAddress, true);
-//        }
-        record memory _record = records[msg.sender];
-        string memory record_date = _record.date;
-        string memory record_brand = _record.brand;
-        address user_address = _record.userAddress;
-        emit RecordCreated(record_date, record_brand, user_address);
+    function accessRecord() public {
+        //        if (records[msg.sender] != null) {
+        //            //Definitely not the right way to emit it
+        //            emit RecordCreated(_date, _type, _userAddress, true);
+        //        }
+        Record[] memory _userRecords = records[msg.sender];
+        for (uint256 i = 0; i < _userRecords.length; i++) {
+            emit EmitRecord(
+                _userRecords[i].date,
+                _userRecords[i].brand,
+                _userRecords[i].userAddress,
+                _userRecords[i].provider
+            );
+        }
+        // emit RecordAccessed(_userRecords);
+        // string memory record_date = _record.date;
+        // string memory record_brand = _record.brand;
+        // address user_address = _record.userAddress;
+        // emit RecordCreated(record_date, record_brand, user_address);
     }
 
     //    function toggleCompleted(uint _id) public {
