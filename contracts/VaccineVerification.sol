69cad4927359d713e5594830f73e5429e4024e70
nex135 <natnevik@gmail.com>
2021-04-13 13:06:07 -0400

    emits event now

--- contracts/VaccineVerification.sol
+++ contracts/VaccineVerification.sol
@@ -5,6 +5,12 @@ contract VaccineVerification {
     address payable owner;
     mapping(address => string) public hashes;
 
+
+    event fileHash(
+        string returnedHash
+    ); 
+
+
     constructor() public {
         owner=msg.sender;
     }
@@ -12,14 +18,14 @@ contract VaccineVerification {
 
     function storeHash(string memory _hash, address user
     ) public {
+        require(msg.sender == owner);
         hashes[user] = _hash;
     }
 
     function getHash() public payable returns(string memory userHash){
         require(msg.value == 1 ether, "You must pay one ether to access the hash.");
         owner.transfer(msg.value);
-        return(hashes[msg.sender]); 
-
+        emit fileHash(hashes[msg.sender]); 
     }
 
 }
